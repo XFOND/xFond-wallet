a8da4bd13cc82c620c73d801c7b206b5996e6ec4
gamalielhere <gamalielgad1234@gmail.com>
2018-04-12 14:30:54 -0700

    Interact with smart contract

--- app/scripts/ethFuncs.js
+++ app/scripts/ethFuncs.js
@@ -1,96 +1,163 @@
-'use strict';
-var ethFuncs = function() {}
+"use strict";
+var ethFuncs = function() {};
 ethFuncs.gasAdjustment = 40;
 ethFuncs.validateEtherAddress = function(address) {
-    if (address.substring(0, 2) != "0x") return false;
-    else if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) return false;
-    else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) return true;
-    else
-        return this.isChecksumAddress(address);
-}
+	if (address.substring(0, 2) != "0x") return false;
+	else if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) return false;
+	else if (
+		/^(0x)?[0-9a-f]{40}$/.test(address) ||
+		/^(0x)?[0-9A-F]{40}$/.test(address)
+	)
+		return true;
+	else return this.isChecksumAddress(address);
+};
 ethFuncs.isChecksumAddress = function(address) {
-    return address == ethUtil.toChecksumAddress(address);
-}
+	return address == ethUtil.toChecksumAddress(address);
+};
 ethFuncs.validateHexString = function(str) {
-    if (str == "") return true;
-    str = str.substring(0, 2) == '0x' ? str.substring(2).toUpperCase() : str.toUpperCase();
-    var re = /^[0-9A-F]+$/g;
-    return re.test(str);
-}
+	if (str == "") return true;
+	str =
+		str.substring(0, 2) == "0x"
+			? str.substring(2).toUpperCase()
+			: str.toUpperCase();
+	var re = /^[0-9A-F]+$/g;
+	return re.test(str);
+};
 ethFuncs.sanitizeHex = function(hex) {
-    hex = hex.substring(0, 2) == '0x' ? hex.substring(2) : hex;
-    if (hex == "") return "";
-    return '0x' + this.padLeftEven(hex);
-}
+	hex = hex.substring(0, 2) == "0x" ? hex.substring(2) : hex;
+	if (hex == "") return "";
+	return "0x" + this.padLeftEven(hex);
+};
 ethFuncs.trimHexZero = function(hex) {
-    if (hex == "0x00" || hex == "0x0") return "0x0";
-    hex = this.sanitizeHex(hex);
-    hex = hex.substring(2).replace(/^0+/, '');
-    return '0x' + hex;
-}
+	if (hex == "0x00" || hex == "0x0") return "0x0";
+	hex = this.sanitizeHex(hex);
+	hex = hex.substring(2).replace(/^0+/, "");
+	return "0x" + hex;
+};
 ethFuncs.padLeftEven = function(hex) {
-    hex = hex.length % 2 != 0 ? '0' + hex : hex;
-    return hex;
-}
+	hex = hex.length % 2 != 0 ? "0" + hex : hex;
+	return hex;
+};
 ethFuncs.addTinyMoreToGas = function(hex) {
-    hex = this.sanitizeHex(hex);
-    return new BigNumber(ethFuncs.gasAdjustment * etherUnits.getValueOfUnit('gwei')).toString(16);
-}
+	hex = this.sanitizeHex(hex);
+	return new BigNumber(
+		ethFuncs.gasAdjustment * etherUnits.getValueOfUnit("gwei")
+	).toString(16);
+};
 ethFuncs.decimalToHex = function(dec) {
-    return new BigNumber(dec).toString(16);
-}
+	return new BigNumber(dec).toString(16);
+};
 ethFuncs.hexToDecimal = function(hex) {
-    return new BigNumber(this.sanitizeHex(hex)).toString();
-}
+	return new BigNumber(this.sanitizeHex(hex)).toString();
+};
 ethFuncs.contractOutToArray = function(hex) {
-    hex = hex.replace('0x', '').match(/.{64}/g);
-    for (var i = 0; i < hex.length; i++) {
-        hex[i] = hex[i].replace(/^0+/, '');
-        hex[i] = hex[i] == "" ? "0" : hex[i];
-    }
-    return hex;
-}
+	hex = hex.replace("0x", "").match(/.{64}/g);
+	for (var i = 0; i < hex.length; i++) {
+		hex[i] = hex[i].replace(/^0+/, "");
+		hex[i] = hex[i] == "" ? "0" : hex[i];
+	}
+	return hex;
+};
 ethFuncs.getNakedAddress = function(address) {
-    return address.toLowerCase().replace('0x', '');
-}
+	return address.toLowerCase().replace("0x", "");
+};
 ethFuncs.getDeteministicContractAddress = function(address, nonce) {
-    nonce = new BigNumber(nonce).toString();
-    address = address.substring(0, 2) == '0x' ? address : '0x' + address;
-    return '0x' + ethUtil.generateAddress(address, nonce).toString('hex');
-}
+	nonce = new BigNumber(nonce).toString();
+	address = address.substring(0, 2) == "0x" ? address : "0x" + address;
+	return "0x" + ethUtil.generateAddress(address, nonce).toString("hex");
+};
 ethFuncs.padLeft = function(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
+	z = z || "0";
+	n = n + "";
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+};
 ethFuncs.getDataObj = function(to, func, arrVals) {
-    var val = "";
-    for (var i = 0; i < arrVals.length; i++) val += this.padLeft(arrVals[i], 64);
-    return {
-        to: to,
-        data: func + val
-    };
-}
+	var val = "";
+	for (var i = 0; i < arrVals.length; i++) val += this.padLeft(arrVals[i], 64);
+	return {
+		to: to,
+		data: func + val
+	};
+};
 ethFuncs.getFunctionSignature = function(name) {
-    return ethUtil.sha3(name).toString('hex').slice(0, 8);
+	return ethUtil
+		.sha3(name)
+		.toString("hex")
+		.slice(0, 8);
 };
 ethFuncs.estimateGas = function(dataObj, callback) {
-    var adjustGas = function(gasLimit) {
-        if (gasLimit == "0x5209") return "21000";
-        if (new BigNumber(gasLimit).gt(4000000)) return "-1";
-        return new BigNumber(gasLimit).toString();
-    }
-    ajaxReq.getEstimatedGas(dataObj, function(data) {
-        if (data.error) {
-            callback(data);
-            return;
-        } else {
-            callback({
-                "error": false,
-                "msg": "",
-                "data": adjustGas(data.data)
-            });
-        }
-    });
+	var adjustGas = function(gasLimit) {
+		if (gasLimit == "0x5209") return "21000";
+		if (new BigNumber(gasLimit).gt(4000000)) return "-1";
+		return new BigNumber(gasLimit).toString();
+	};
+	ajaxReq.getEstimatedGas(dataObj, function(data) {
+		if (data.error) {
+			callback(data);
+			return;
+		} else {
+			callback({
+				error: false,
+				msg: "",
+				data: adjustGas(data.data)
+			});
+		}
+	});
+};
+
+function sizeHex(bytes) {
+	return bytes * 2;
 }
+
+function trim(str) {
+	return str.replace(/\0[\s\S]*$/g, "");
+}
+
+function getAscii(hex) {
+	hex = hex.substring(0, 2) === "0x" ? hex : "0x" + hex;
+	return trim(ethUtil.toAscii(hex));
+}
+
+ethFuncs.decode = function(hex) {
+	let tokens = [];
+	hex = hex.substring(0, 2) === "0x" ? hex.substring(2) : hex;
+	hex = hex.substring(0, hex.lastIndexOf("1") - 1); //starting point
+	let offset = hex.length;
+	offset -= sizeHex(32);
+	let countTokens = hex.substr(offset, sizeHex(32));
+	offset -= sizeHex(1);
+	let isName = parseInt(hex.substr(offset, sizeHex(1)));
+	offset -= sizeHex(1);
+	let isWebSite = parseInt(hex.substr(offset, sizeHex(1)));
+	offset -= sizeHex(1);
+	let isEmail = parseInt(hex.substr(offset, sizeHex(1)));
+	let numTokens = new BigNumber("0x" + countTokens).toNumber();
+
+	for (var i = 0; i < parseInt(numTokens); i++) {
+		var token = {};
+		offset -= sizeHex(16);
+		token.symbol = getAscii(hex.substr(offset, sizeHex(16)));
+		offset -= sizeHex(20);
+		token.addr = "0x" + hex.substr(offset, sizeHex(20));
+		offset -= sizeHex(8);
+		token.decimals = new BigNumber("0x" + hex.substr(offset, sizeHex(8))).toNumber();
+		offset -= sizeHex(32);
+		token.balance = new BigNumber("0x" + hex.substr(offset, sizeHex(32))).toFixed();
+		if (isName) {
+			offset -= sizeHex(16);
+			token.name = getAscii(hex.substr(offset, sizeHex(16)));
+		}
+		if (isWebSite) {
+			offset -= sizeHex(32);
+			token.website = getAscii(hex.substr(offset, sizeHex(32)));
+		}
+		if (isEmail) {
+			offset -= sizeHex(32);
+			token.email = getAscii(hex.substr(offset, sizeHex(32)));
+		}
+		tokens.push(token);
+	}
+	return tokens;
+};
+
 module.exports = ethFuncs;
---
+++ app/scripts/tokenContract.js
@@ -0,0 +1,54 @@
+const tokenInterface = require('./tokens/tokenContractAbi.json');
+const tokenContract = function () {
+  this.tokensAbi = {};
+  for (var i in tokenInterface) this.tokensAbi[tokenInterface[i].name] = tokenInterface[i];
+}
+
+tokenContract.prototype.getDataString = function(func, inputs) {
+  var fullFuncName = ethUtil.solidityUtils.transformToFullName(func);
+  var funcSig = ethFuncs.getFunctionSignature(fullFuncName);
+  var typeName = ethUtil.solidityUtils.extractTypeName(fullFuncName);
+  var types = typeName.split(',');
+  types = types[0] === "" ? [] : types;
+  return '0x' + funcSig + ethUtil.solidityCoder.encodeParams(types, inputs);
+};
+
+/**
+ * tokenContract.prototype.getAllBalance - description
+ *
+ * @param  {type} to       description
+ * @param  {string address, boolean name, boolean website, boolean email, integer count} params
+ * @param  {type} callback description
+ * @return {type}          description
+ */
+tokenContract.prototype.getAllBalance = function(to, params) {
+  const address = params.address ? params.address : '';
+  const name = params.name ? params.name : true;
+  const website = params.website ? params.website : true;
+  const email = params.email ? params.email : true;
+  const count = params.count ? params.count : 0;
+
+  ajaxReq.getEthCall({
+    to: to,
+    data: this.getDataString(this.tokensAbi.getAllBalance, [address, name, website, email, count])
+  }, function(data) {
+    if(data.error || data.data === '0x') console.log(data, "error");
+    else {
+      return ethFuncs.decode(data.data);
+    };
+  })
+};
+
+tokenContract.prototype.getToken = function(to, address) {
+  ajaxReq.getEthCall({
+    to: to,
+    data: this.getDataString(this.tokensAbi.getToken, [address])
+  }, function(data) {
+    if(data.error || data.data === '0x') console.log(data, "error");
+    else {
+      return ethFuncs.decode(data.data);
+    };
+  });
+}
+
+module.exports = tokenContract;
